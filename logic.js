5301c22b84149012820ffe643e110c67136d1652
Katherine Morriss <40501252+kathmorriss@users.noreply.github.com>
2019-02-02 12:27:51 -0600

    map layers which isn't working :)

--- logic.js
+++ logic.js
@@ -371,3 +371,45 @@ L.circle([39.0587, -94.5986], {
   fillOpacity: 0.75,
   radius: 500
 }).addTo(myMap);
+
+
+// Add all the cityMarkers to a new layer group.
+// Now we can handle them as one group instead of referencing each individually
+var busLayer = L.layerGroup(busStops);
+
+// Define variables for our tile layers
+var light = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.light",
+  accessToken: API_KEY
+});
+
+var dark = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.dark",
+  accessToken: API_KEY
+});
+
+// Only one base layer can be shown at a time
+var baseMaps = {
+  Light: light,
+  Dark: dark
+};
+
+// Overlays that may be toggled on or off
+var overlayMaps = {
+  Buses: busLayer
+};
+
+// Create map object and set default layers
+var myMap = L.map("map", {
+  center: [46.2276, 2.2137],
+  zoom: 6,
+  layers: [light, busLayer]
+});
+
+// Pass our map layers into our layer control
+// Add the layer control to the map
+L.control.layers(baseMaps, overlayMaps).addTo(myMap);
